8369deb2795fcde5b7f6840b832df0a3248f5015
SANS002 <sans9361639086@gmail.com>
2024-04-08 12:25:04 +0530

    quizz

---
+++ quizz game/quizz game.py
@@ -0,0 +1,16 @@
+print("WELCOM TO QUIZZ!")
+
+playing = input("do you want play this game ..?")
+
+print(playing)
+
+if playing !="yes":
+    quit()
+
+print("OK! Let's play :")
+answer = input("What does CPU stand for ?")
+if answer =="central processing unit":
+    print(correct!)
+else:
+    print("Icorrect!")
+    
