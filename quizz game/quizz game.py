38a2a9b10ee6cee170e16d10f12cdce5a2f8e5de
SANS002 <sans9361639086@gmail.com>
2024-04-10 10:50:38 +0530

    adding points

--- quizz game/quizz game.py
+++ quizz game/quizz game.py
@@ -1,16 +1,48 @@
-print("WELCOM TO QUIZZ!")
+print("WELCOME TO QUIZZ!")
 
 playing = input("do you want play this game ..?")
 
 print(playing)
 
-if playing !="yes":
+x=0
+
+if playing.lower() !="yes":
     quit()
 
 print("OK! Let's play :")
 answer = input("What does CPU stand for ?")
-if answer =="central processing unit":
-    print(correct!)
+if answer.lower() =="central processing unit":
+    print("correct!")
+    x=x+1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for ?")
+if answer.lower() =="graphics processing unit":
+    print("correct!")
+    x=x+1
+else:
+    print("Incorrect!")  
+
+answer = input( "What does RAM stand for ?")
+if answer.lower() =="random access memory":
+    print("correct!")
+    x=x+1
+else:
+    print("Incorrect!")
+
+answer = input("What does ROM stand for ?")
+if answer.lower() =="read only memory":
+    print("correct!")
+    x=x+1
 else:
     print("Icorrect!")
-    
+
+answer = input("Is python is programming language ?")
+if answer.lower() =="yes":
+    print("correct!")
+    x=x+1
+else:
+    print("Incorrect!")
+
+print("your points out of 5 is :",x)
