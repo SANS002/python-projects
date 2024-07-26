fe7fe2133a7afb9a1badae28b8a3d32f871c7c42
SANS002 <sans9361639086@gmail.com>
2024-07-26 22:10:06 +0530

    datavisualizer

--- datavisualizer/dashboard.py
+++ datavisualizer/dashboard.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -87,6 +88,50 @@ st.subheader("line chart of sales and profit")
 linechart = pd.DataFrame(filtered_df.groupby(filtered_df["Profit"])["Sales"].sum()).reset_index()
 fig2 = px.line(linechart, x = "Sales",y="Profit" ,labels={"Sales:" "Profit"},height=500, width= 1000, template="gridon")
 st.plotly_chart(fig2,use_container_width=True)
+
+with st.expander("View Data for sales and profit"):
+     st.write(linechart.style.background_gradient(cmap = 'Oranges'))
+     csv = linechart.to_csv(index= False).encode('utf-8')
+     st.download_button("Download data", data=csv , mime='text/csv')
+
+st.subheader("Hierarchical view of Sales using TreeMap")
+fig3 = px.treemap(filtered_df,path=["Region","Category"], values='Sales',hover_data= ['Sales'],
+                color ="Sub-Category")
+fig3.update_layout(width=800,height=650)
+st.plotly_chart(fig3,use_container_width= True)
+
+chart1,chart2= st.columns(2)
+with chart1:
+    st.subheader('segment wise sales')
+    fig = px.pie(filtered_df,values="Sales",names="Segment",template ="plotly_dark")
+    fig.update_traces(text = filtered_df['Segment'],textposition= 'inside')
+    st.plotly_chart(fig,use_container_width=True)
+with chart2:
+    st.subheader('Category wise sales')
+    fig = px.pie(filtered_df,values="Sales",names="Category",template ="gridon")
+    fig.update_traces(text = filtered_df['Category'],textposition= 'inside')
+    st.plotly_chart(fig,use_container_width=True)
+
+st.subheader(":point_right: Sales summary")
+with st.expander("summary table"):
+    df_sample = df[0:5][["Region","Sales","Profit","Category","City","Quantity"]]
+    fig = ff.create_table(df_sample,colorscale="Cividis")
+    st.plotly_chart(fig,use_container_width=True)
+
+data1 = px.scatter(filtered_df,x= "Sales",y='Profit',size= "Quantity")
+data1['layout'].update(title ="Relationship between Sales and profit using Scatter plot",
+                       titlefont = dict(size = 20 ),xaxis = dict(title = "Sales",titlefont = dict(size=19)),
+                       yaxis = dict(title="Profit",titlefont = dict(size = 19)))
+
+st.plotly_chart(data1,use_container_width= True)
+
+with st.expander('View your entaire dataset'):
+    st.write(filtered_df.iloc[:500,1:20:2].style.background_gradient(cmap="Blues"))
+
+csv = df.to_csv(index = False).encode('utf-8')
+st.download_button("download data",data = csv,mime='text/csv')
+
+    
 
 
 
